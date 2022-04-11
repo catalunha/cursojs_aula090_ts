ded919fce32f71b820feac673953901c50bf9fd0
catalunha <catalunha.mj@gmail.com>
2022-04-11 19:54:46 -0300

    aulas

---
+++ src/aula110_classes2/index.ts
@@ -0,0 +1,35 @@
+export class Empresa {
+  // public readonly nome: string = 'Ana';
+  readonly nome: string;
+  private readonly colaboradores: Colaborador[] = [];
+  protected readonly cnpj: string;
+  constructor(nome: string, cnpj: string) {
+    this.nome = nome;
+    this.cnpj = cnpj;
+  }
+  addColaborador(colaborador: Colaborador): void {
+    this.colaboradores.push(colaborador);
+  }
+  mostrarColadorador(): void {
+    for (const col of this.colaboradores) {
+      console.log(col);
+    }
+  }
+  getNome(): string {
+    return this.nome;
+  }
+}
+export class Colaborador {
+  constructor(public readonly nome: string, public readonly snome: string) {}
+}
+const empresa1 = new Empresa('Brintec', '11.111.111/0001-11');
+console.log('empresa1 = ', empresa1);
+console.log('empresa1.nome = ', empresa1.nome);
+const colaborador1 = new Colaborador('Ana', 'Aninha...');
+const colaborador2 = new Colaborador('Bia', 'Biazinha...');
+empresa1.addColaborador(colaborador1);
+console.log('empresa1 = ', empresa1);
+empresa1.addColaborador(colaborador2);
+console.log('empresa1 = ', empresa1);
+empresa1.mostrarColadorador();
+console.log('empresa1.getNome() = ', empresa1.getNome());
